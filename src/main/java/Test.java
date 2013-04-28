9f5eada0d2851bf8b265f87e6e78ac2bd7061a82
syevle <syevle@gmail.com>
2013-04-28 21:27:47 +0530

    dfgdfg

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,18 +1,34 @@
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 public class Test {
-	public static void main(String[] args) {
+		 public static void main(String[] args){
+		      System.out.println(getCapitalizeFirstLetter("I am boy."));
+		  }
+
+
+		 public static String getCapitalizeFirstLetter(String text) {
+				StringBuffer sb = new StringBuffer();
+				try {
+					String test = text;
+					String[] testSplit = test.split(" ");
+					
+					for (int i = 0; i < testSplit.length; i++) {
+						String capitalVer = capitalizeFirstLetter(testSplit[i]);
+						if (i != 0) {
+							sb.append(" ");
+						}
+						sb.append(capitalVer);
+					}
+					return sb.toString();
+				} catch (Exception es) {
+
+				}
+				return sb.toString();
+			}
+
+			public static String capitalizeFirstLetter(final String string) {
+				if (string == null || string.equals(""))
+					throw new NullPointerException("no string ");
+
+				return Character.toUpperCase(string.charAt(0)) + string.substring(1);
+			}
 		 
-		   DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-		   //get current date time with Date()
-		   Date date = new Date();
-		   System.out.println(dateFormat.format(date));
-	 
-		   //get current date time with Calendar()
-		   Calendar cal = Calendar.getInstance();
-		   System.out.println(dateFormat.format(cal.getTime()));
-	 
-	  }
-}
+		}
--- src/main/java/com/city/util/ApplicationUtil.java
+++ src/main/java/com/city/util/ApplicationUtil.java
@@ -11,4 +11,31 @@ public class ApplicationUtil {
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
+
+	public static String getCapitalizeFirstLetter(String text) {
+		StringBuffer sb = new StringBuffer();
+		try {
+			String test = text;
+			String[] testSplit = test.split(" ");
+
+			for (int i = 0; i < testSplit.length; i++) {
+				String capitalVer = capitalizeFirstLetter(testSplit[i]);
+				if (i != 0) {
+					sb.append(" ");
+				}
+				sb.append(capitalVer);
+			}
+			return sb.toString();
+		} catch (Exception es) {
+
+		}
+		return sb.toString();
+	}
+
+	public static String capitalizeFirstLetter(final String string) {
+		if (string == null || string.equals(""))
+			throw new NullPointerException("no string ");
+
+		return Character.toUpperCase(string.charAt(0)) + string.substring(1);
+	}
 }
